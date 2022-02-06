17032078e9df75814032f69651420751e18bce19
Berk Karaal <karaalberk7@gmail.com>
2022-02-07 00:16:54 +0300

    change STATICFILES_STORAGE

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
